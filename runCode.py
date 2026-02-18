a06fa80e8e722b3cc52ddfe938324ce4acfd4fbf
Faidat Egberinde <faidategberinde@gmail.com>
2026-02-18 15:29:27 +0100

    Added runCode file

---
+++ runCode.py
@@ -0,0 +1,54 @@
+print("Hello, welcome to the adder!")
+
+def add(a, b):
+    return a + b
+
+number1 = 10
+number2 = 20
+
+print("Adding: " + str(number1) + " and " + str(number2))
+sum = add(number1, number2)
+
+print("The sum is: " + str(sum))
+
+
+
+# # Top-to-bottom execution + reassignment
+# print("\nExample 1")
+# x = 5
+# print(x)
+
+# x = 10
+# print(x)
+
+# # Defining vs calling a function
+# print("\nExample 2")
+# def greet():
+#     print("Hello!")
+
+# greet()
+
+# # Passing data in + getting a result back
+# print("\nExample 3")
+# def add(a, b):
+#     return a + b
+
+# result = add(3, 4)
+# print(result)
+
+# print("\nExample 4")
+# def say_hi():
+#     print("Hi!")
+
+# def start():
+#     say_hi()
+
+# start()
+
+# # Compute → store → then use later
+# print("\nExample 5")
+# def square(n):
+#     return n * n
+
+# value = square(5)
+# print(value + 10)
