a15b789cc338f678791d17c78853d3649d69607a
Faidat Egberinde <faidategberinde@gmail.com>
2026-03-10 04:56:02 +0100

    Added Species Catalogue homepage and FastAPI route

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 # server.py
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
 from request import search_species
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/hello")
 async def hello():
@@ -13,3 +17,7 @@ async def species(search: str = Query(..., description="Species name to search f
     print("I got: " + search)
     extracted_data = search_species(search)
     return {"data": extracted_data}
+
+@app.get("/", response_class=HTMLResponse)
+async def return_site(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
