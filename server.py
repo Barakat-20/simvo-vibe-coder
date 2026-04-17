77e67cc1be49cd1516cd1c5567b8c596c000a862
Faidat Egberinde <faidategberinde@gmail.com>
2026-04-17 07:17:57 +0100

    Added cors polict to server for localhost access

--- server.py
+++ server.py
@@ -3,8 +3,23 @@ from fastapi import FastAPI, Query, Request
 from request import search_species
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://simvo-vibe-coder-faidat.vercel.app"
+]
+
+app.add_middleware(
+     CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 templates = Jinja2Templates(directory="templates")
 
